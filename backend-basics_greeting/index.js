a59ed6f6a77e3bee78a0a33a08e334a44c7d2b5e
Bruno <bruno.dill@protonmail.com>
2024-02-02 11:20:53 +0100

    solved greeting challenge

---
+++ backend-basics_greeting/index.js
@@ -0,0 +1,5 @@
+import { server } from "./server.js";
+
+const port = 8000;
+
+server.listen(port, () => console.log(port));
---
+++ backend-basics_greeting/server.js
@@ -0,0 +1,5 @@
+import { createServer } from "node:http";
+
+export const server = createServer((request, response) => {
+  response.end("Hello, Marie, Phillip & Bruno!");
+});
