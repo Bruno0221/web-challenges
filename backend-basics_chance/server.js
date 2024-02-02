d81f8ffd8bbcbfda351df94ca075d4d6138ee0ea
Bruno <bruno.dill@protonmail.com>
2024-02-02 11:47:45 +0100

    route challenge solved

--- backend-basics_chance/server.js
+++ backend-basics_chance/server.js
@@ -1,3 +1,17 @@
 import { createServer } from "node:http";
+import { Chance } from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+const name = chance.name();
+const age = chance.age();
+const profession = chance.profession({ rank: false });
+
+export const server = createServer((request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.end(
+      `Hello, my name is ${name} and I am ${age} years old. I am a ${profession}.`
+    );
+  }
+});
